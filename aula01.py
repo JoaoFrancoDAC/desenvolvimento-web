cd73e25ffd9eb7d367af10f46f7e9e314a121fd0
JoaoFrancoDAC <142028379+JoaoFrancoDAC@users.noreply.github.com>
2023-08-21 08:56:22 -0300

    Add files via upload
    Primeira aula de desenvolvimento web.

---
+++ aula01.py
@@ -0,0 +1,25 @@
+-python
+https://www.python.org/
+#Python é uma linguagem de programação de alto nível, interpretada e pseudo-compilada que pode ser utilizada para diversas tarefas no desenvolvimento web
+#desde criar a lógica do ersátilservidor até automatizar tarefas e interagir com recursos externos (fazer requisições, executar frameworks)
+
+-git
+https://git-scm.com/
+#Git é uma ferramenta para auxiliar o versionamento e compartilhação de codigos. Em relação a desenvolvimento web, ele permite com que você visualize melhor a evolução do seu projeto
+#e auxilie o trabalho de uma equipe em cima de um mesmo projeto 
+
+-vscore
+https://code.visualstudio.com/
+#vscode é simplesmente uma IDE. Ela serve para facílitar a escritura e leitura do código.
+
+-chrome
+https://www.google.com/intl/pt-PT/chrome/
+
+-firefox
+https://www.mozilla.org/en-US/firefox/windows/
+
+-microsoft edge
+https://www.microsoft.com/pt-br/edge/download?form=MA13FJ
+
+#As 3 ferramentas acima são chamadas de navegadores web. Que são aplicativos de software projetados para permitir que os usuários
+#acessem, visualizem e interajam com conteúdo na World Wide Web
